800e742d24398fb40f4d5700651fa757ab23590c
Debesh <debeshghorui@gmail.com>
2025-10-23 20:15:59 +0530

    give the initial styling the hero section

--- src/landingPage/home/Hero.js
+++ src/landingPage/home/Hero.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className='container p-5 mb-5'>
+      <div className='row text-center'>
+        <img 
+          src="media/homeHero.png" 
+          alt="Hero" 
+          className='mb-5'
+        />
+
+        <h1 className='mt-5'>Invest in everything</h1>
+
+        <p>Oline platform to invest in stocks, derivative, mutual funds, and more</p>
+
+        <button 
+          className='btn btn-primary p-3 fs-5 mt-5 mb-5' 
+          style={{ 
+              width: "20%", margin: "0 auto" 
+            }} 
+        >Signup now</button>
+      </div>
     </div>
-  )
+  );
 }
