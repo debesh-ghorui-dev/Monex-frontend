4f2d124dedfce53c6e9ee9021a0ed4da733148bc
Debesh <debeshghorui@gmail.com>
2025-10-23 20:17:13 +0530

    create Award component

--- src/landingPage/home/Awards.js
+++ src/landingPage/home/Awards.js
@@ -2,8 +2,51 @@ import React from 'react'
 
 export default function Awards() {
   return (
-    <div>
-      <h1>Awards</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-6 p-5'>
+          <img src="media/largestBroker.svg" alt="largest Broker" />
+        </div>
+        <div className='col-6 p-5 mt-3'>
+          <h1>Largest stock broker in India</h1>
+          <p 
+            className='mb-5'
+          >2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
+          <div className="row">
+            <div className='col-6'>
+              <ul>
+                <li>
+                  <p>Futures and Options</p>
+                </li>
+                <li>
+                  <p>Commodity derivatives</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className='col-6'>
+              <ul>
+                <li>
+                  <p>Stocks & IPOs</p>
+                </li>
+                <li>
+                  <p>Direct mutual funds</p>
+                </li>
+                <li>
+                  <p>Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img 
+            src="media/pressLogos.png" 
+            alt="" 
+            style={{width: "90%"}}
+          />
+        </div>
+      </div>
     </div>
   )
 }
