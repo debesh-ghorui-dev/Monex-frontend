e82ff3523465a90b2b49219ca3ae156cb7ba150f
Debesh <debeshghorui@gmail.com>
2025-11-03 10:07:42 +0530

    Add routes for holdings and positions data retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,23 @@
 import 'dotenv/config';
 import app from './app.js';
+import holdingsModel from './model/holdingsModel.js';
+import positionsModel from './model/positionsModel.js'
 
 const port = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/holdings', async(req, res) => {
+    let data = await holdingsModel.find({});
+    res.json(data)
+})
+
+app.get('/positions', async(req, res) => {
+    let data = await positionsModel.find({});
+    res.json(data)
 })
 
 app.listen(port, () => {
